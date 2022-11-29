3762f005bf70bf4b23f8af814f7faee696eb0b11
Laurence Dyer <ljdyer@gmail.com>
2022-11-29 05:47:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ REQUIREMENTS = [
 
 setup(
     name='bilstm_char_feature_restorer',
-    version='0.39',
+    version='1.0',
     description="""\
 Train character-level BiLSTM models for restoration of features such as \
 spaces, punctuation, and capitalization to unformatted texts\
